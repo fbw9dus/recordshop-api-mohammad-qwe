2dbe35b30613bd1d6bed14b939e4d10066ba3f5b
mohammad saied <mohammadsolingen@gmail.com>
2020-03-24 12:20:03 +0100

    test

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,37 +1,37 @@
-const UserModel = require('./../models/User')
-exports.getUsers = (req, res, next) => {
+const User = require('../models/User')
+
+exports.getUsers = async (req, res, next) => {
   // Schreib hier code um alle Kunden aus der users-Collection zu holen
-  var users = UserModel.find()
-  res.status(200).send(users);
+  const users =await User.find()
+  res.status(200).send(users)
 };
 
-exports.getUser = (req, res, next) => {
-  const { id } = req.params;
+exports.getUser = async (req, res, next) => {
+  const id  = req.params.id;
   // Schreib hier code um den Kunden mit der id aus params aus der users-Collection zu holen
-  var user = UserModel.findById(id)
+  const user =await User.findById(id)
   res.status(200).send(user);
 };
 
-exports.deleteUser = (req, res, next) => {
-  const { id } = req.params;
+exports.deleteUser = async (req, res, next) => {
+  const { id } = req.params.id;
   // Schreib hier code um den Kunden mit der id aus params aus der users-Collection zu löschen
-  const user = UserModel.findByIdAndDelete(id)
+  const user =await User.findByIdAndDelete(id)
   res.status(200).send(user);
 };
 
-exports.updateUser = (req, res, next) => {
-  const { id } = req.params;
+exports.updateUser = async (req, res, next) => {
+  const { id } = req.params.id;
   const dt = req.body;
   // Schreib hier code um den User mit der id aus params in der users-Collection mit den Daten aus req.body zu aktualisieren
-  var user = UserModel.findByIdAndUpdate(id,dt)
+  const user =await User.findByIdAndUpdate(id,dt)
   res.status(200).send(user);
 };
 
-exports.addUser =async (req, res, next) => {
-  const user = req.body;
+exports.addUser = async (req, res, next) => {
+  const userData = req.body;
   // Schreib hier code um die Daten des neuen Kunden aus req.body in der users-Collection zu speichern
-  const newUser = new UserModel(user)
-  await newUser.save()
+  
+  const user =await new User(userData)
   res.status(200).send(user);
 };
-
